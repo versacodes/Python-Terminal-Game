d12752e1116ecd4e97cd2544735060825c690969
Franz <franz@antix1>
2022-10-28 14:22:32 +0800

    Added loop for title screen.

---
+++ text_adventure.py
@@ -0,0 +1,17 @@
+# The Adventure Game
+
+print("The Adventure Game")
+print("1 - Start\n2 - Quit")
+
+while True:
+	try:
+		title_screen_choice = int(input("Choose an option above: "))
+	except ValueError:
+		print("Enter only a number from the options above.")
+		continue
+	break
+
+	if title_screen_choice == 2:
+		print("Exited the game.")
+	elif title_screen_choice > 2 or title_screen_choice < 1:
+		print("Choose between 1 or 2 only.")
